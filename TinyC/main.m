7463adbfac8c70d1796b8d0e565682c9c08d61da
Tom Cole <tom@nomad.na.sas.com>
2014-04-11 11:53:42 -0400

    Initial Commit

---
+++ TinyC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TinyC
+//
+//  Created by Tom Cole on 4/11/14.
+//  Copyright (c) 2014 Forest Edge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
